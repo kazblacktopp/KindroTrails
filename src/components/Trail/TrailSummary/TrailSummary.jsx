41756e4a04ec32c28c7eb65321f48efe5731c7a0
kazblacktopp <karen.blacktopp@gmail.com>
2022-10-29 15:48:51 +1000

    Add styles for tablet devices

--- src/components/Trail/TrailSummary/TrailSummary.jsx
+++ src/components/Trail/TrailSummary/TrailSummary.jsx
@@ -15,45 +15,54 @@ function TrailSummary(props) {
         The Cradle Mountain-Lake St Clair National Park is part of the
         magnificent Tasmanian Wilderness World Heritage Area.
       </p>
-      <div>
-        <ul className={classes.callout}>
+      <a
+        href="https://parks.tas.gov.au/explore-our-parks/cradle-mountain/overland-track"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Learn more
+      </a>
+      <div className={classes["callout-container"]}>
+        <div className={classes.callout}>
           <h3>Fun Facts</h3>
-          <li>
-            <strong>Distance</strong>: 65km (6 days)
-          </li>
-          <li>
-            <strong>Direction</strong>: One way
-          </li>
-          <li>
-            <strong>Difficulty</strong>: Grade 4
-          </li>
-          <li>
-            <strong>Carry Own Gear?</strong>: Yes
-          </li>
-          <li>
-            <strong>Environment</strong>: Remote Alpine
-          </li>
-          <li>
-            <strong>Elevation</strong>: 720m to 1,250m
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul className={classes.callout}>
+          <ul>
+            <li>
+              <strong>Distance</strong>: 65km (6 days)
+            </li>
+            <li>
+              <strong>Direction</strong>: One way
+            </li>
+            <li>
+              <strong>Difficulty</strong>: Grade 4
+            </li>
+            <li>
+              <strong>Carry Own Gear?</strong>: Yes
+            </li>
+            <li>
+              <strong>Environment</strong>: Remote Alpine
+            </li>
+            <li>
+              <strong>Elevation</strong>: 720m to 1,250m
+            </li>
+          </ul>
+        </div>
+        <div className={classes.callout}>
           <h3>Average Temperatures</h3>
-          <li>
-            <strong>Summer</strong>: 5.8&#176;C min. 18.7&#176;C max.
-          </li>
-          <li>
-            <strong>Autumn</strong>: 3.0&#176;C min. 13.4&#176;C max.
-          </li>
-          <li>
-            <strong>Winter</strong>: 0.1&#176;C min. 7.7&#176;C max.
-          </li>
-          <li>
-            <strong>Spring</strong>: 2.2&#176;C min. 12.9&#176;C max.
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <strong>Summer</strong>: 5.8&#176;C min. 18.7&#176;C max.
+            </li>
+            <li>
+              <strong>Autumn</strong>: 3.0&#176;C min. 13.4&#176;C max.
+            </li>
+            <li>
+              <strong>Winter</strong>: 0.1&#176;C min. 7.7&#176;C max.
+            </li>
+            <li>
+              <strong>Spring</strong>: 2.2&#176;C min. 12.9&#176;C max.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
