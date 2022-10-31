99fb2c1f742b4bac70a73b36662c92d0d97748a4
kazblacktopp <karen.blacktopp@gmail.com>
2022-10-31 20:45:39 +1000

    #17-1 Add styles

--- src/components/Trail/TrailPage/TrailPage.jsx
+++ src/components/Trail/TrailPage/TrailPage.jsx
@@ -1,16 +1,15 @@
-import { Fragment } from "react";
-import PhotoGallery from "../TrailGallery/PhotoGallery/PhotoGallery";
-import GearList from "../../Gear/GearList/GearList";
 import TrailSummary from "../TrailSummary/TrailSummary";
+
 import classes from "./TrailPage.module.css";
 
 function TrailPage(props) {
   return (
-    <Fragment>
-      <TrailSummary />
-      <PhotoGallery />
-      <GearList />
-    </Fragment>
+    <main className={classes["container-outer"]}>
+      <section className={classes["container-inner"]}>
+        <h2>Overland Track</h2>
+        <TrailSummary />
+      </section>
+    </main>
   );
 }
 
