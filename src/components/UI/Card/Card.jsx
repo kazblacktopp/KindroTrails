c8d4fa5ec609892961e08ba6d9a908db236349eb
kazblacktopp <karen.blacktopp@gmail.com>
2022-11-09 18:07:10 +1000

    Create Card module with styles

---
+++ src/components/UI/Card/Card.jsx
@@ -0,0 +1,9 @@
+import classes from "./Card.module.css";
+
+function Card({ styles, children }) {
+  const { card } = classes;
+  const cardClasses = `${styles} ${card}`;
+  return <div className={cardClasses}>{children}</div>;
+}
+
+export default Card;
