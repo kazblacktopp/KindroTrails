601aa772972b807022d58bc7e40e86bc98015fb7
kazblacktopp <karen.blacktopp@gmail.com>
2022-11-18 14:00:15 +1000

    Create config file

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+// Settings for UploadFile.js
+export const IMAGE_MAX_WIDTH = 1600;
+export const THUMBNAIL_MAX_WIDTH = 200;
+export const IS_THUMBNAIL = true;
+
+// Settings for ImageCompressor.js
+export const DEFAULT_SCALE = 70;
+export const DEFAULT_QUALITY = 70;
