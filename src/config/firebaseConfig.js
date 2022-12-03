05794ca82ec4dd8e144fe19314077b29b91e1ca8
kazblacktopp <karen.blacktopp@gmail.com>
2022-12-03 12:22:40 +1000

    Setup Firebase as backend

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC-tsFYnKtGLtOykF2PXU4aEQRhqwqj8Y8',
+  authDomain: 'kindrotrails.firebaseapp.com',
+  projectId: 'kindrotrails',
+  storageBucket: 'kindrotrails.appspot.com',
+  messagingSenderId: '1013262370858',
+  appId: '1:1013262370858:web:48df471720750bde2fdb53',
+};
