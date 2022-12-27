d0ce74dccc61438320ee99e67bb676a7f595e9d6
kazblacktopp <karen.blacktopp@gmail.com>
2022-12-27 10:02:32 +1000

    Update page to accept data through props

--- src/components/Trail/TrailPage/TrailPage.jsx
+++ src/components/Trail/TrailPage/TrailPage.jsx
@@ -1,15 +1,23 @@
-import TrailSummary from "../TrailSummary/TrailSummary";
-import PhotoGallery from "../../PhotoGallery/PhotoGallery";
-import classes from "./TrailPage.module.css";
+import TrailSummary from '../TrailSummary/TrailSummary';
+import PhotoGallery from '../../PhotoGallery/PhotoGallery';
+import classes from './TrailPage.module.css';
 
 function TrailPage(props) {
+  const { title, country, state, trailImages, ...remainingData } =
+    props.trailData;
+
   return (
     <main>
-      <div className={classes["section-container"]}>
-        <h1 className={classes["page_title"]}>Overland Track</h1>
-        <div className={classes["top-section_container"]}>
-          <TrailSummary />
-          <PhotoGallery />
+      <div className={classes['section-container']}>
+        <div className={classes['title_container']}>
+          <h1>{title}</h1>
+          <p>
+            {state}, {country}
+          </p>
+        </div>
+        <div className={classes['top-section_container']}>
+          <TrailSummary summaryData={remainingData} />
+          <PhotoGallery photos={trailImages} />
         </div>
       </div>
     </main>
