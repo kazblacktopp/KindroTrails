beb6b53bb878893c4ce776481d00449522e812ed
kazblacktopp <karen.blacktopp@gmail.com>
2023-01-09 11:30:24 +1000

    Move databaseURL to firebaseConfig file

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,6 +3,8 @@ export const firebaseConfig = {
   authDomain: 'kindrotrails.firebaseapp.com',
   projectId: 'kindrotrails',
   storageBucket: 'kindrotrails.appspot.com',
+  databaseURL:
+    'https://kindrotrails-default-rtdb.asia-southeast1.firebasedatabase.app',
   messagingSenderId: '1013262370858',
   appId: '1:1013262370858:web:48df471720750bde2fdb53',
 };
