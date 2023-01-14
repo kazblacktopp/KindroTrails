da314ec89555fde22ef9084ac3a63341227cab82
kazblacktopp <karen.blacktopp@gmail.com>
2023-01-14 21:53:54 +1000

    Create function for uploading data to database

--- src/hooks/use-database.js
+++ src/hooks/use-database.js
@@ -5,17 +5,24 @@ import {
   uploadString,
   getDownloadURL,
 } from 'firebase/storage';
-import { getDatabase, ref as createRefInDatabase } from 'firebase/database';
+import {
+  getDatabase,
+  ref as createRefInDatabase,
+  update,
+} from 'firebase/database';
 import { firebaseConfig } from '../config/firebaseConfig';
 import { useState } from 'react';
 
 export function useDatabase() {
-  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
 
   async function uploadToStorage(imageDataObj, trailID, isThumbnail = false) {
+    setError(null);
+    setIsLoading(true);
+
     const { name, image, width, height } = imageDataObj;
 
     const storage = getStorage(app);
@@ -35,27 +42,70 @@ export function useDatabase() {
     };
 
     try {
-      const snapshot = await uploadString(
+      const uploadResult = await uploadString(
         imageFileRef,
         image,
         'data_url',
         fileMetadata
       );
 
-      const url = await getDownloadURL(imageFileRef);
+      if (!uploadResult) {
+        throw new Error('No upload snapshot returned!');
+      }
 
-      return { snapshot, url };
+      const url = await getDownloadURL(uploadResult.ref);
+
+      if (!url) {
+        throw new Error('No URL returned for uploaded image.');
+      }
+
+      const { metadata } = uploadResult;
+
+      setIsLoading(false);
+
+      return { url, metadata };
     } catch (error) {
-      console.error(error);
-      setError('Something went wrong!' + error.message);
+      console.error(`uploadToStorage: ${error}`);
+      setIsLoading(false);
+      setError(`Failed to upload image!`);
     }
   }
 
-  function uploadToDatabase(dataObj, dbBucket) {
-    setError('');
-    console.log(createRefInDatabase, database);
-    console.log('Uploading new trail');
+  async function updateDatabase(trailObj) {
+    setError(null);
+    setIsLoading(true);
+
+    const { id, country, state } = trailObj;
+
+    const trailCountry = country.split(' ').join('_');
+    const trailState = state.split(' ').join('_');
+
+    const updates = {};
+
+    updates['/trails/' + id] = trailObj;
+    updates[
+      '/trailLocations/' + trailCountry + '/' + trailState + '/' + id
+    ] = true;
+
+    const database = getDatabase(app);
+    const dbRef = createRefInDatabase(database);
+
+    try {
+      const updateResult = await update(dbRef, updates);
+
+      if (updateResult) {
+        throw new Error(updateResult);
+      }
+
+      setIsLoading(false);
+
+      return true;
+    } catch (error) {
+      console.error(`updateDatabase: ${error}`);
+      setIsLoading(false);
+      setError(`Failed to upload Trail. ${error.message}`);
+    }
   }
 
-  return { error, uploadToStorage, uploadToDatabase };
+  return { uploadToStorage, updateDatabase, isLoading, error };
 }
