3b5dbc87974010f86fd65cf18fd70949f56a9288
kazblacktopp <karen.blacktopp@gmail.com>
2023-01-14 21:46:05 +1000

    Create context to store app data

--- src/components/NewTrail/NewTrail.jsx
+++ src/components/NewTrail/NewTrail.jsx
@@ -1,36 +1,61 @@
-import { useState } from 'react';
-import useDatabase from '../../hooks/use-database';
+import { Fragment, useState, useContext } from 'react';
+import { useDatabase } from '../../hooks/use-database';
+import TrailContext from '../../store/trail-context';
+import Card from '../UI/Card/Card';
 import NewTrailForm from './NewTrailForm';
 
-export default function NewTrail() {
-  const [newTrail, setNewTrail] = useState({});
-  const { error, uploadToStorage, uploadToDatabase } = useDatabase();
+export default function NewTrail({ onClose, onViewTrail }) {
+  const { uploadToStorage, updateDatabase, isLoading, error } = useDatabase();
+  const [trailName, setTrailName] = useState('');
+  const [newTrailID, setNewTrailID] = useState(null);
+  const [message, setMessage] = useState(null);
+  const trailCxt = useContext(TrailContext);
 
   async function uploadNewTrailHandler(newTrailData, newTrailImages) {
-    setNewTrail(newTrailData);
+    setMessage(null);
+    setTrailName(newTrailData.title);
 
     const trailID = newTrailData.title.toLowerCase().split(' ').join('_');
 
-    const uploadedImagesArray = await uploadImages(newTrailImages, trailID);
+    const lowercaseTitle = newTrailData.title.toLowerCase();
+    const lowercaseCountry = newTrailData.country.toLowerCase();
+    const lowercaseState = newTrailData.state.toLowerCase();
 
-    setNewTrail(prevTrailState => {
-      const newTrailObj = {
-        ...prevTrailState,
-        trailImages: [...uploadedImagesArray],
-      };
+    const uploadedImagesArray = await uploadImage(newTrailImages, trailID);
 
-      return newTrailObj;
-    });
+    const newTrailObj = {
+      ...newTrailData,
+      id: trailID,
+      title: lowercaseTitle,
+      country: lowercaseCountry,
+      state: lowercaseState,
+      trailImages: [...uploadedImagesArray],
+    };
 
-    // Upload newTrail to database
-    uploadToDatabase(newTrail, 'trails');
+    // TODO: Set newTrailObj to a Context instead of component state as 'currentTrail'? and load it as the current trail
+    // setNewTrail(() => {
+    //   console.log('Setting New Trail after upload to storage...');
+    //   console.log(newTrailObj);
 
-    // uploadToDatabase(newTrail, 'trailLocations');
+    //   return newTrailObj;
+    // });
 
-    // Upload newTrailLocation to database
+    const updateIsSuccessful = await updateDatabase(newTrailObj);
+
+    if (!updateIsSuccessful) return;
+
+    console.log('Upload is complete');
+
+    trailCxt.addToTrails(newTrailObj);
+
+    setNewTrailID(trailID);
+
+    setMessage('Trail uploaded successfully!');
   }
 
-  async function uploadImages(imageDataArray, trailID) {
+  async function uploadImage(imageDataArray, trailID) {
+    console.log('Starting to upload images to storage...');
+
     const uploadedImagesObjArray = await Promise.all(
       imageDataArray.map(async imageObj => {
         const { imageFullscreen, imageThumbnail, imageAttribution } = imageObj;
@@ -48,24 +73,24 @@ export default function NewTrail() {
           isThumbnail
         );
 
-        const { url: fullscreenURL, snapshot: fullscreenSnapshot } =
+        const { url: fullscreenURL, metadata: fullscreenMetadata } =
           uploadedFullscreenData;
 
-        const { url: thumbnailURL, snapshot: thumbnailSnapshot } =
+        const { url: thumbnailURL, metadata: thumbnailMetadata } =
           uploadedThumbnailData;
 
         const uploadedImageObj = {
           imageFullscreen: {
             image: fullscreenURL,
             name: imageFullscreen.name,
-            size: fullscreenSnapshot.metadata.size,
+            size: fullscreenMetadata.size,
             width: imageFullscreen.width,
             height: imageFullscreen.height,
           },
           imageThumbnail: {
             image: thumbnailURL,
             name: imageThumbnail.name,
-            size: thumbnailSnapshot.metadata.size,
+            size: thumbnailMetadata.size,
             width: imageThumbnail.width,
             height: imageThumbnail.height,
           },
@@ -76,24 +101,65 @@ export default function NewTrail() {
       })
     );
 
+    console.log('Finished uploading images to storage.');
+
     return uploadedImagesObjArray;
   }
 
-  // const { country, state, name } = newTrail;
-  // Upload newTrail to database using trail name, modified with underscores, as 'id'
-  // Use below format for trailLocation data:
-  // const newTrailLocation = {
-  //   [country]: {
-  //     [state]: {
-  //       [id]: true,
-  //     },
-  //   },
-  // };
+  function viewTrailHandler() {
+    setMessage(null);
+    onViewTrail(newTrailID);
+  }
 
-  return (
-    <div>
-      {!error && <NewTrailForm onSubmitNewTrail={uploadNewTrailHandler} />}
-      {error && <p>{error}</p>}
-    </div>
+  function closeMessageHandler() {
+    setMessage(null);
+    onClose();
+  }
+
+  let content = (
+    <NewTrailForm onSubmitNewTrail={uploadNewTrailHandler} onClose={onClose} />
   );
+
+  if (isLoading && !error && !message) {
+    content = (
+      <div className="msg_card">
+        <Card>
+          <p>{`Uploading ${trailName} ...`}</p>
+        </Card>
+      </div>
+    );
+  }
+
+  if (message && !error && !isLoading) {
+    content = (
+      <div className="msg_card">
+        <Card>
+          <p>{message}</p>
+          <div className="action_btns">
+            <button className="btn btn_red" onClick={closeMessageHandler}>
+              Close
+            </button>
+            <button className="btn btn_green" onClick={viewTrailHandler}>
+              View Trail
+            </button>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+
+  if (error) {
+    content = (
+      <div className="msg_card">
+        <Card>
+          <p>{`Something went wrong: ${error}`}</p>
+          <button className="btn btn_red" onClick={closeMessageHandler}>
+            Close
+          </button>
+        </Card>
+      </div>
+    );
+  }
+
+  return <Fragment>{content}</Fragment>;
 }
