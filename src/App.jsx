843042d8f0f2362bdb886ee14e3856291a6e0b80
kazblacktopp <karen.blacktopp@gmail.com>
2023-01-16 16:09:48 +1000

    Add header element containing NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from 'react';
 import TrailContext from './store/trail-context';
+import NavBar from './components/UI/Nav/NavBar';
+import SearchOptions from './components/Search/SearchOptions';
 import TrailPage from './components/Trail/TrailPage/TrailPage';
 import NewTrail from './components/NewTrail/NewTrail';
 
@@ -9,12 +11,25 @@ function App() {
   const [viewTrail, setViewTrail] = useState(false);
   const [selectedTrail, setSelectedTrail] = useState(null);
 
+  // Set showAddBtn to true if user is logged in and has admin permissions
+  const [showAddBtn, setShowAddBtn] = useState(true);
+
+  function clickHomeHandler() {
+    setAddNewTrail(false);
+    setViewTrail(false);
+
+    // If user is logged in and has admin permissions:
+    setShowAddBtn(true);
+  }
+
   function addNewTrailHandler() {
     setAddNewTrail(true);
+    setShowAddBtn(false);
   }
 
   function closeNewTrailFormHandler() {
     setAddNewTrail(false);
+    setShowAddBtn(true);
   }
 
   function viewTrailHandler(trailID) {
@@ -26,28 +41,44 @@ function App() {
     setViewTrail(true);
   }
 
+  let appPages;
+
+  if (!viewTrail && !addNewTrail) {
+    appPages = <SearchOptions></SearchOptions>;
+  }
+
+  if (viewTrail && !addNewTrail) {
+    appPages = (
+      <TrailPage
+        trailData={selectedTrail}
+        trailImages={selectedTrail.trailImages}
+      />
+    );
+  }
+
+  if (addNewTrail) {
+    appPages = (
+      <NewTrail
+        onClose={closeNewTrailFormHandler}
+        onViewTrail={viewTrailHandler}
+      />
+    );
+  }
+
   return (
     <div className="app_container">
-      {!addNewTrail && (
-        <button
-          className="new_trail_btn btn btn_blue"
-          onClick={addNewTrailHandler}
-        >
-          + Add New Trail
-        </button>
-      )}
-      {viewTrail && !addNewTrail && (
-        <TrailPage
-          trailData={selectedTrail}
-          trailImages={selectedTrail.trailImages}
-        />
-      )}
-      {addNewTrail && (
-        <NewTrail
-          onClose={closeNewTrailFormHandler}
-          onViewTrail={viewTrailHandler}
-        />
-      )}
+      <header className="top_header">
+        <NavBar onClickHome={clickHomeHandler}></NavBar>
+        {!addNewTrail && showAddBtn && (
+          <button
+            className="new_trail_btn btn btn_blue"
+            onClick={addNewTrailHandler}
+          >
+            Add New Trail
+          </button>
+        )}
+      </header>
+      {appPages}
     </div>
   );
 }
