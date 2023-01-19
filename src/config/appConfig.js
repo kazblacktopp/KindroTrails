5d048591823f49266e722a435d51deed5bed6b78
kazblacktopp <karen.blacktopp@gmail.com>
2023-01-20 08:27:28 +1000

    Add new form inputs for gathering image attribution

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,3 +2,32 @@
 export const IMAGE_MAX_WIDTH = 1600;
 export const THUMBNAIL_MAX_WIDTH = 200;
 export const THUMBNAIL = true;
+
+// Variables for newTrail form inputs
+
+export const CC_LICENSE_NAMES = {
+	NAME_PDM: 'Public Domain Mark (PDM 1.0)',
+	NAME_CC0: 'Public Domain Dedication-Universal (CC0 1.0)',
+	NAME_CC_BY: 'Attribution (CC BY)',
+	NAME_CC_BY_SA: 'Attribution-ShareAlike (CC BY-SA)',
+	NAME_CC_BY_NC: 'Attribution-NonCommercial (CC BY-NC)',
+	NAME_CC_BY_NC_SA: 'Attribution-NonCommercial ShareAlike (CC BY-NC-SA)',
+	NAME_CC_BY_NC_ND: 'Attribution-NonCommercial-NoDerivatives (CC BY-NC-ND)',
+	NAME_CC_BY_ND: 'Attribution-NoDerivatives (CC BY-ND)',
+};
+
+export const CC_LICENSE_BASE_URLS = {
+	URL_PDM: 'https://creativecommons.org/publicdomain/mark/',
+	URL_CC0: 'https://creativecommons.org/publicdomain/zero/',
+	URL_CC_BY: 'https://creativecommons.org/licenses/by/',
+	URL_CC_BY_SA: 'https://creativecommons.org/licenses/by-sa/',
+	URL_CC_BY_NC: 'https://creativecommons.org/licenses/by-nc/',
+	URL_CC_BY_NC_SA: 'https://creativecommons.org/licenses/by-nc-sa/',
+	URL_CC_BY_NC_ND: 'https://creativecommons.org/licenses/by-nc-nd/',
+	URL_CC_BY_ND: 'https://creativecommons.org/licenses/by-nd/',
+};
+
+export const CC_LICENSE_LATEST_VERSION = '4.0';
+
+export const CC_LICENSE_PDM_VERSION = '1.0';
+
