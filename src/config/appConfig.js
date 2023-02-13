e265a613badf99f54b19d1477e0a99e0b0f3fde5
kazblacktopp <karen.blacktopp@gmail.com>
2023-02-13 15:24:38 +1000

    Increase thumbnail max width to 300px

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,6 @@
 // Configuration variables for compressFile.js
 export const IMAGE_MAX_WIDTH = 1600;
-export const THUMBNAIL_MAX_WIDTH = 200;
+export const THUMBNAIL_MAX_WIDTH = 300;
 export const THUMBNAIL = true;
 
 // Variables for newTrailForm component form inputs
