0f0512764d969210c6e4d57a11782d2092967250
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-30 08:41:42 +1000

    Refactor styles for <main> element

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './UI/Nav/NavBar';
-import classes from './RootLayout.module.css';
 
 export default function RootLayout() {
 	return (
 		<>
 			<NavBar />
-			<main className={classes.content}>
+			<main>
 				<Outlet />
 			</main>
 		</>
