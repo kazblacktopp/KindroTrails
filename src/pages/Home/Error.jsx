2a3007b491f6ecc58a325ba3d3732255d6ad1990
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-30 08:42:01 +1000

    Create Error component

---
+++ src/pages/Home/Error.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../../components/UI/Nav/NavBar';
+
+export default function Error() {
+	return (
+		<>
+			<NavBar />
+			<main>
+				<h1>An Error Has Occurred</h1>
+				<p>Could not find this page.</p>
+			</main>
+		</>
+	);
+}
