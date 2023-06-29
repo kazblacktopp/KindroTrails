e57407fe2ad73bbab7700287f297f4dbfee97c45
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-29 13:13:24 +1000

    Add routing and refactor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState, useRef } from 'react';
-import NavBar from './components/UI/Nav/NavBar.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './components/RootLayout.js';
+import Home from './pages/Home/Home.jsx';
 import SearchOptions from './components/Search/SearchOptions';
 import CountryListPage from './components/Search/SearchByCountry/CountryList/CountryListPage';
 import StateListPage from './components/Search/SearchByCountry/StateList/StateListPage';
@@ -8,114 +9,25 @@ import TrailPage from './components/Trail/TrailPage/TrailPage';
 import NewTrail from './components/NewTrail/NewTrail';
 
 export default function App() {
-	const [addNewTrail, setAddNewTrail] = useState(false);
-	const [viewTrail, setViewTrail] = useState(false);
-	const [viewCountriesList, setViewCountriesList] = useState(false);
-	const [viewStatesList, setViewStatesList] = useState(false);
-	const [viewTrailsList, setViewTrailsList] = useState(false);
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <RootLayout />,
+			children: [
+				{ path: '/', element: <Home /> },
+				{ path: '/search', element: <SearchOptions /> },
+				{ path: '/search/country', element: <CountryListPage /> },
+				{ path: 'search/country/state', element: <StateListPage /> },
+				{
+					path: 'search/country/state/trail',
+					element: <TrailListPage />,
+				},
+				{ path: 'trail', element: <TrailPage /> },
+				{ path: 'new-trail', element: <NewTrail /> },
+			],
+		},
+	]);
 
-	const selectedCountry = useRef(null);
-	const selectedState = useRef(null);
-	const selectedTrail = useRef(null);
-
-	function clickHomeHandler() {
-		setAddNewTrail(false);
-		setViewTrail(false);
-		setViewCountriesList(false);
-		setViewStatesList(false);
-	}
-
-	function addNewTrailHandler() {
-		setAddNewTrail(true);
-	}
-
-	function closeNewTrailFormHandler() {
-		setAddNewTrail(false);
-	}
-
-	function displayResultHandler(resultType, resultID = null) {
-		if (resultType === 'countries') {
-			setViewCountriesList(true);
-		}
-
-		if (resultType === 'states') {
-			setViewCountriesList(false);
-			setViewStatesList(true);
-			selectedCountry.current = resultID;
-		}
-
-		if (resultType === 'trails') {
-			setViewStatesList(false);
-			setViewTrailsList(true);
-			selectedState.current = resultID;
-		}
-	}
-
-	function viewTrailHandler(trail) {
-		setViewTrailsList(false);
-		setAddNewTrail(false);
-		setViewTrail(true);
-
-		selectedTrail.current = trail;
-	}
-
-	let appPages = (
-		<SearchOptions onResult={displayResultHandler}></SearchOptions>
-	);
-
-	if (viewCountriesList) {
-		appPages = <CountryListPage onResult={displayResultHandler} />;
-	}
-
-	if (viewStatesList) {
-		appPages = (
-			<StateListPage
-				selectedCountry={selectedCountry.current}
-				onResult={displayResultHandler}
-			/>
-		);
-	}
-
-	if (viewTrailsList) {
-		appPages = (
-			<TrailListPage
-				selectedCountry={selectedCountry.current}
-				selectedState={selectedState.current}
-				onResult={viewTrailHandler}
-			/>
-		);
-	}
-
-	if (viewTrail) {
-		const { trailImages, ...trailData } = selectedTrail.current;
-
-		appPages = (
-			<TrailPage trailData={trailData} trailImages={trailImages} />
-		);
-	}
-
-	if (addNewTrail) {
-		appPages = (
-			<NewTrail
-				onClose={closeNewTrailFormHandler}
-				onViewTrail={viewTrailHandler}
-			/>
-		);
-	}
-
-	return (
-		<>
-			<header>
-				<NavBar
-					onClickHome={clickHomeHandler}
-					onClickAddTrail={addNewTrailHandler}
-					isAuth={isAuthenticated}
-				/>
-			</header>
-			<main>{appPages}</main>
-			<footer></footer>
-		</>
-	);
+	return <RouterProvider router={router} />;
 }
 
