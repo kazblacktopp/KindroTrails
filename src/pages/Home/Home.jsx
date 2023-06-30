b9aff7c1cfd46379c6007027070ccd480f38c3e0
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-30 18:24:10 +1000

    Refactor: moved section styles to index.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom';
-import classes from './Home.module.css';
 
 export default function Home() {
-	const { section } = classes;
-
 	return (
-		<section className={section}>
+		<section>
 			<h1>Welcome to Kindro Trails!</h1>
 			<p>
 				Where you can find information and recommended gear lists for
