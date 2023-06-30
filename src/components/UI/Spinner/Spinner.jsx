3aac570b8a0829a85695920d6367230e3d87989d
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-30 20:47:06 +1000

    Refactor: updated styles for main and section elements

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -1,12 +1,12 @@
 import icons from '../../../assets/icons.svg';
 import classes from './Spinner.module.css';
 
-export default function Spinner(props) {
+export default function Spinner() {
 	const { spinner } = classes;
 
 	return (
-		<div className="spinner">
-			<svg className={spinner}>
+		<div className={spinner}>
+			<svg>
 				<use href={`${icons}#icon-loader`}></use>
 			</svg>
 		</div>
