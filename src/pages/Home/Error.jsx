561aded331fecb0d0eb5fc238a962ede9aac68aa
kazblacktopp <karen.blacktopp@gmail.com>
2023-07-03 12:51:04 +1000

    Add logic for showing conditional error messages

--- src/pages/Home/Error.jsx
+++ src/pages/Home/Error.jsx
@@ -1,12 +1,27 @@
-import NavBar from '../../components/UI/Nav/NavBar';
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../../components/UI/Nav/NavBar';
 
-export default function Error() {
+export default function ErrorPage() {
+	const error = useRouteError();
+
+	let title = 'An error cccurred!';
+	let message = 'Something went wrong!';
+
+	if (error.status === 404) {
+		title = 'Page not found!';
+		message = 'The page you are looking for does not exist.';
+	}
+
+	if (error.status === 500) {
+		message = error.data.message;
+	}
+
 	return (
 		<>
-			<NavBar />
+			<MainNavigation />
 			<main>
-				<h1>An Error Has Occurred</h1>
-				<p>Could not find this page.</p>
+				<h1>{title}</h1>
+				<p>{message}</p>
 			</main>
 		</>
 	);
