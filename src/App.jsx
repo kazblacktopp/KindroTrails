07242142e87fbbe16f878a0160d9f5033ad1309e
kazblacktopp <karen.blacktopp@gmail.com>
2023-07-03 13:02:34 +1000

    Add route loader function for CountryListPage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import RootLayout from './components/RootLayout';
 import Home from './pages/Home/Home.jsx';
 import SearchOptions from './components/Search/SearchOptions';
-import CountryListPage from './components/Search/SearchByCountry/CountryList/CountryListPage';
+import CountryListPage, {
+	loader as countryListLoader,
+} from './components/Search/SearchByCountry/CountryList/CountryListPage';
 import StateListPage from './components/Search/SearchByCountry/StateList/StateListPage';
 import TrailListPage from './components/Search/SearchByCountry/TrailList/TrailListPage';
 import TrailPage from './components/Trail/TrailPage/TrailPage';
 import NewTrail from './components/NewTrail/NewTrail';
-import Error from './pages/Home/Error.jsx';
+import ErrorPage from './pages/Home/Error.jsx';
 import About from './pages/Home/About';
 import Contact from './pages/Home/Contact';
 
@@ -16,13 +18,17 @@ export default function App() {
 		{
 			path: '/',
 			element: <RootLayout />,
-			errorElement: <Error />,
+			errorElement: <ErrorPage />,
 			children: [
 				{ index: true, element: <Home /> },
 				{ path: 'search', element: <SearchOptions /> },
 				{ path: 'about', element: <About /> },
 				{ path: 'contact', element: <Contact /> },
-				{ path: 'search/country', element: <CountryListPage /> },
+				{
+					path: 'search/country',
+					loader: countryListLoader,
+					element: <CountryListPage />,
+				},
 				{
 					path: 'search/:countryId',
 					element: <StateListPage />,
