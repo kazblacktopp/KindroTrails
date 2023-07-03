b17eada7080b4c57833e881f8f2935ed7e9c58f4
kazblacktopp <karen.blacktopp@gmail.com>
2023-07-03 12:56:13 +1000

    Update component with Spinner

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom';
-import NavBar from './UI/Nav/NavBar';
+import { Outlet, useNavigation } from 'react-router-dom';
+import MainNavigation from './UI/Nav/NavBar';
+import Spinner from './UI/Spinner/Spinner';
 
 export default function RootLayout() {
+	const navigation = useNavigation();
+
+	const isLoading = navigation.state === 'loading';
+
 	return (
 		<>
-			<NavBar />
-			<main>
-				<Outlet />
-			</main>
+			<MainNavigation />
+			<main>{isLoading ? <Spinner /> : <Outlet />}</main>
 		</>
 	);
 }
