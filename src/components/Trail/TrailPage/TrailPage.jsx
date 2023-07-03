63875aa6a82e7d9b1156278dbec349616d4d584f
kazblacktopp <karen.blacktopp@gmail.com>
2023-07-03 13:06:25 +1000

    Add logic to retrieve trailData from redux store

--- src/components/Trail/TrailPage/TrailPage.jsx
+++ src/components/Trail/TrailPage/TrailPage.jsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import TrailSummary from '../TrailSummary/TrailSummary';
 import PhotoGallery from '../PhotoGallery/PhotoGallery';
 import RecommendedGearList from '../RecommendedGear/RecommendedGearList';
@@ -5,9 +7,24 @@ import capitaliseString from '../../../helpers/capitaliseString';
 
 import classes from './TrailPage.module.css';
 
-function TrailPage({ trailData, trailImages }) {
-	const { title, country, state, recommendedGear, ...summaryData } =
-		trailData;
+function TrailPage({ trailData = null, images = null }) {
+	const { trailId } = useParams();
+
+	const { trails } = useSelector(state => state.trailData);
+
+	let trail = trails[trailId];
+
+	if (trailData) {
+		trail = trailData;
+	}
+
+	let { trailImages } = trail;
+
+	if (images) {
+		trailImages = images;
+	}
+
+	const { title, country, state, recommendedGear, ...summaryData } = trail;
 
 	const capitalisedTitle = capitaliseString(title);
 	const capitalisedCountry = capitaliseString(country);
